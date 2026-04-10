b1553e6cf2626e97f3a035ef70699d1a41420803
Pranay Kapoor <pranay.kapoor@myndsol.com>
2026-04-10 16:51:11 +0530

    First V1 of Angular Practice File

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { User } from './user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-app');
+
+  
+  
 }
